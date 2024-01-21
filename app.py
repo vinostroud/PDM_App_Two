95add5e60505103b0272cc34ff5b4a34cbbcef4a
vinostroud <134710724+vinostroud@users.noreply.github.com>
2024-01-21 11:36:04 -0500

    Formatted tooltips

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import folium
 from streamlit_folium import folium_static
 
 
+
 # List of empires
 empires = [
     "Upper Egypt",
@@ -41,17 +42,35 @@ gdf = gpd.read_file(geojson_path)
 st.title("Select An Empire")
 selected_empire = st.selectbox("Select an Empire", gdf['Empire'].tolist())
 
-# Display an empty map
+# Set the size of the map display
+map_height = 1000
 st.title("World Map")
-m = folium.Map(location=[0, 0], zoom_start=5)
+m = folium.Map(location=[0, 0], zoom_start=5, control_scale=True, height=map_height)
+
 
 #add function to pull geojson data based on select box.
-
 def plot_geojson():
     selected_gdf = gdf[gdf['Empire'] == selected_empire]
+    
+    #center map around selected geodataframe
     center_lat, center_lon = selected_gdf.geometry.centroid.y.mean(), selected_gdf.geometry.centroid.x.mean()
     m.location = [center_lat, center_lon]
-    folium.GeoJson(selected_gdf, name=selected_empire, tooltip=selected_empire).add_to(m)
+# Iterate through GeoJSON features and add them to the map
+    for feature in selected_gdf.iterfeatures():
+        geojson_feature = folium.GeoJson(feature, name=selected_empire)
+        
+        # Convert negative years to BC
+        start_date = -feature['properties']['Starting_Date']
+        end_date = -feature['properties']['End_Date']
+        
+        # Create a formatted tooltip with line breaks
+        tooltip_text = f"<b>Empire:</b> {feature['properties']['Empire']}<br>" \
+                       f"<b>Start Date:</b> {start_date} BC<br>" \
+                       f"<b>End Date:</b> {end_date} BC"
+        
+        folium.Popup(tooltip_text, max_width=300).add_to(geojson_feature)  # Adjust max_width as needed
+        
+        geojson_feature.add_to(m)
 
 plot_geojson()
     
@@ -60,5 +79,3 @@ plot_geojson()
 
 
 folium_static(m)
-
-tooltip = 'This is a placeholder tooltip'
